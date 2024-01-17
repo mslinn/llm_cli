c7d9accbe8505305df7eb42d1dc2513953545f8a
Mike Slinn <mslinn@mslinn.com>
2024-01-17 13:33:04 -0500

    -

--- lib/llm_cli.rb
+++ lib/llm_cli.rb
@@ -4,10 +4,11 @@ require_relative 'llm_cli/version'
 
 def require_subdirectory(dir)
   Dir[File.join(dir, '*.rb')].each do |file|
-    require file unless file.end_with?('/llm_cli.rb')
+    require file unless file == __FILE__
   end
 end
 
+puts __FILE__
 require_subdirectory File.realpath(__dir__) # Require all Ruby files in 'lib/', except this file
 Pathname(__dir__).children.select(&:directory?).each do |directory|
   require_subdirectory directory.to_s
